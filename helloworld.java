b063bd72020d2222720add142993d3d8eccf5d22
Rutujapuranikdkte <rutujapuranik7@gmail.com>
2025-06-30 21:47:35 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+package com.dkte;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		int i=1;
+	
+		while(i<=5)
+		{
+		System.out.println("hello world");
+		i++;
+		}
+}
+}
